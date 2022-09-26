4ab7db8fd5362aea64a346328c4d32dddd70cc43
mcservery.eu <artyom.czech@gmail.com>
2022-09-26 01:05:12 +0000

    r

--- replit.nix
+++ replit.nix
@@ -1,6 +1,7 @@
 { pkgs }: {
 	deps = [
-		pkgs.nodejs-16_x
+		pkgs.gnupg1orig
+  pkgs.nodejs-16_x
         pkgs.nodePackages.typescript-language-server
         pkgs.nodePackages.yarn
         pkgs.replitPackages.jest
